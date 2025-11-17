5af616e5f26096c983bd32d84585c33a6634c3fb
Greg Newman <greg@gnuman.localdomain>
2025-11-17 10:10:57 -0500

    wezterm and ghostty

---
+++ home/config/wezterm/wezterm.lua
@@ -0,0 +1,105 @@
+-- Import the wezterm module
+local wezterm = require 'wezterm'
+-- Creates a config object which we will be adding our config to
+local config = wezterm.config_builder()
+
+-- Core Config
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = true
+config.audible_bell = "Disabled"
+config.tab_max_width = 100
+
+-- How many lines of scrollback you want to retain per tab
+config.scrollback_lines = 10000
+
+-- Theme
+-- Find them here: https://wezfurlong.org/wezterm/colorschemes/index.html
+config.color_scheme = 'Dracula (Official)'
+-- config.color_scheme = 'Solarized'
+config.window_background_opacity = 1.0
+config.inactive_pane_hsb = {
+  saturation = 0.5,
+  brightness = 0.6,
+}
+config.command_palette_rows = 10
+config.show_new_tab_button_in_tab_bar = false
+config.colors = {
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = '#000000',
+  }
+}
+config.window_padding = {
+  left = '8',
+  right = '8',
+  top = '0cell',
+  bottom = '0cell',
+}
+
+-- Fonts
+config.font = wezterm.font({ family = 'Fira Code' })
+-- And a font size that won't have you squinting
+config.font_size = 13
+
+-- Powerline
+local function segments_for_right_status(window)
+  return {
+    window:active_workspace(),
+    wezterm.strftime('%a %b %-d %H:%M'),
+    wezterm.hostname(),
+  }
+end
+
+wezterm.on('update-status', function(window, _)
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  local segments = segments_for_right_status(window)
+
+  local color_scheme = window:effective_config().resolved_palette
+  -- Note the use of wezterm.color.parse here, this returns
+  -- a Color object, which comes with functionality for lightening
+  -- or darkening the colour (amongst other things).
+  local bg = wezterm.color.parse(color_scheme.background)
+  local fg = color_scheme.foreground
+
+  -- Each powerline segment is going to be coloured progressively
+  -- darker/lighter depending on whether we're on a dark/light colour
+  -- scheme. Let's establish the "from" and "to" bounds of our gradient.
+  local gradient_to, gradient_from = bg
+  gradient_from = gradient_to:lighten(0.2)
+
+
+  -- Yes, WezTerm supports creating gradients, because why not?! Although
+  -- they'd usually be used for setting high fidelity gradients on your terminal's
+  -- background, we'll use them here to give us a sample of the powerline segment
+  -- colours we need.
+  local gradient = wezterm.color.gradient(
+    {
+      orientation = 'Horizontal',
+      colors = { gradient_from, gradient_to },
+    },
+    #segments -- only gives us as many colours as we have segments.
+  )
+
+  -- We'll build up the elements to send to wezterm.format in this table.
+  local elements = {}
+
+  for i, seg in ipairs(segments) do
+    local is_first = i == 1
+
+    if is_first then
+      table.insert(elements, { Background = { Color = 'none' } })
+    end
+    table.insert(elements, { Foreground = { Color = gradient[i] } })
+    table.insert(elements, { Text = SOLID_LEFT_ARROW })
+
+    table.insert(elements, { Foreground = { Color = fg } })
+    table.insert(elements, { Background = { Color = gradient[i] } })
+    table.insert(elements, { Text = ' ' .. seg .. ' ' })
+  end
+
+  window:set_right_status(wezterm.format(elements))
+end)
+
+-- Returns our config to be evaluated. We must always do this at the bottom of this file
+return config
